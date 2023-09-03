7ebe03fa33c6e5159513c17968774212796a01a1
ninad patil <ninad.patil198@gmail.com>
2023-09-03 22:25:38 +0530

    added rate limiter for api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
+const { rateLimit } = require("express-rate-limit");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extende: true }));
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { InfoController } = require("../../controllers");
-const { authRequestMiddleware } = require("../../middlewares");
+//const { InfoController } = require("../../controllers");
+//const { authRequestMiddleware } = require("../../middlewares");
 const router = express.Router();
 const userRouter = require("./user-routes");
 
-router.get("/info", authRequestMiddleware.checkAuth, InfoController.info);
+//router.get("/info", authRequestMiddleware.checkAuth, InfoController.info);
 router.use("/user", userRouter);
 module.exports = router;
+
+//uncomment the line to demonstrate the authentication for api
