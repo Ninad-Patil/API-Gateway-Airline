89b2cefff9fe6043a26454dfbf3e14b9df644467
ninad patil <ninad.patil198@gmail.com>
2023-09-03 22:51:18 +0530

    added reverse-proxy with a simple npm package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { rateLimit } = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
@@ -9,10 +10,25 @@ const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
   max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 });
-app.use(limiter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extende: true }));
 
+app.use(limiter);
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+  })
+);
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
